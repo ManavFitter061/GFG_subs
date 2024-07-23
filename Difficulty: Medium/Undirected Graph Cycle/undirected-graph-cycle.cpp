f34c984f24d2881f94ebec210dcd192b78d6b9da
ManavFitter061 <149518610+ManavFitter061@users.noreply.github.com>
2024-07-23 15:17:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -6,61 +6,66 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool dfs(int i,vector<int> adj[],vector<int> &vis){
+    bool dfs(int i,int parent,vector<int> adj[],vector<int> &vis){
         vis[i]=1;
         for(int it:adj[i]){
-            if(vis[it]==1){
-                return 1;
+            if(!vis[it]){
+                if(dfs(it,i,adj,vis)==true) return true;
             }
-            else dfs(it,adj,vis);
+            else if(it!=parent){
+                return true;
+            }
         }
         return false;
         
     }
-    bool bfs(int i,vector<int> adj[],vector<int> &vis){
-        queue<pair<int,int>> q;
-        q.push({i,-1});
-        vis[i]=1;
-        
-        while(!q.empty()){
-            int curr=q.front().first;
-            int prev=q.front().second;
-            q.pop();
-            
-            for(auto it:adj[curr]){
-                if(!vis[it]){
-                    vis[it]=1;
-                    q.push({it,curr});
-                }
-                else if(it!=prev){
-                    return 1;
-                }
-            }
+    
+    bool isCycle(int V, vector<int> adj[]) {
+        // Code here
+        vector<int> vis(V,0);
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+            if(dfs(i,-1,adj,vis)==1){
+                return 1;
+            }}
         }
         return 0;
     }
-    // bool isCycle(int V, vector<int> adj[]) {
-    //     // Code here
+    
+    
+    // bool bfs(int i,vector<int> adj[],vector<int> &vis){
+    //     queue<pair<int,int>> q;
+    //     q.push({i,-1});
+    //     vis[i]=1;
         
-    //     for(int i=0;i<V;i++){
-    //         vector<int> vis(V,0);
-    //         if(dfs(i,adj,vis)==1){
-    //             return 1;
+    //     while(!q.empty()){
+    //         int curr=q.front().first;
+    //         int prev=q.front().second;
+    //         q.pop();
+            
+    //         for(auto it:adj[curr]){
+    //             if(!vis[it]){
+    //                 vis[it]=1;
+    //                 q.push({it,curr});
+    //             }
+    //             else if(it!=prev){
+    //                 return 1;
+    //             }
     //         }
     //     }
     //     return 0;
     // }
-    bool isCycle(int V, vector<int> adj[]) {
-        vector<int>vis(V,0);
-        for(int i=0;i<V;i++){
-            if(vis[i]==0){
-                if(bfs(i,adj,vis)==1){
-                    return 1;
-                }
-            }
-        }
-        return 0;
-    }
+    // bool isCycle(int V, vector<int> adj[]) {
+    //     vector<int>vis(V,0);
+    //     for(int i=0;i<V;i++){
+    //         if(vis[i]==0){
+    //             if(bfs(i,adj,vis)==1){
+    //                 return 1;
+    //             }
+    //         }
+    //     }
+    //     return 0;
+    // }
     
 };
 
